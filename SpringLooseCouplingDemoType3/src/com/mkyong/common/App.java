673f83a884ae05ed6ba143c07ce83d8017d458e8
Gururaj <gyk27@yahoo.com>
2015-03-12 12:55:42 +0530

    rrr

--- SpringLooseCouplingDemoType3/src/com/mkyong/common/App.java
+++ SpringLooseCouplingDemoType3/src/com/mkyong/common/App.java
@@ -1,17 +1,17 @@
 package com.mkyong.common;
- 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
- 
+
 import com.mkyong.output.OutputHelper;
- 
-public class App 
+
+public class App 100
 {
     public static void main( String[] args )
     {
-    	ApplicationContext context = 
+    	ApplicationContext context =
     	   new ClassPathXmlApplicationContext(new String[] {"Spring-Common.xml"});
- 
+
     	OutputHelper output = (OutputHelper)context.getBean("OutputHelper");
     	output.generateOutput();
 
